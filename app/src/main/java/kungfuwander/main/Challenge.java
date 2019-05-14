c83516630e272c7a7d5bf922ea07c83592f79d83
sofia1208 <sofia.schmidmair@live.de>
2019-05-14 17:50:32 +0200

    fragment+camera

--- app/src/main/java/kungfuwander/main/Challenge.java
+++ app/src/main/java/kungfuwander/main/Challenge.java
@@ -1,4 +1,13 @@
 package kungfuwander.main;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.view.View;
+
 public class Challenge {
+    public void create()
+    {
+
+    }
 }
--- app/src/main/java/kungfuwander/main/ProfilFragment.java
+++ app/src/main/java/kungfuwander/main/ProfilFragment.java
@@ -2,6 +2,7 @@ package kungfuwander.main;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -12,23 +13,62 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-public class ProfilFragment extends Fragment{
+public class ProfilFragment extends Fragment implements View.OnClickListener {
 
     private static final int CAMERA_REQUEST = 1888;
     private ImageView imageView;
     private static final int MY_CAMERA_PERMISSION_CODE = 100;
 
-    @Nullable
+
     @Override
-    public View onCreateView( LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, null);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+
+       imageView = (ImageView) v.findViewById(R.id.imageProfilePicture);
+        imageView.setOnClickListener(this);
+        return v;
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.imageProfilePicture:
+
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
 
 
+                break;
+        }
+    }
 
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,  String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_PERMISSION_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            } else {
+
+            }
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK)
+        {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
+    }
 }
