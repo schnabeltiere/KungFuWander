df7b3e439c7a2da39120119d156b7d12e069043c
snowLimit <sven.vucak2@hotmail.com>
2019-05-22 17:45:15 +0200

    FIX hiking is null
    fixed that user cannot stop hiking which has not yet started. have to look what happens when user closes app...
    also cleaned the code a bit
    ADD ability to change name

--- app/src/main/java/kungfuwander/main/ReplacementForFriends.java
+++ app/src/main/java/kungfuwander/main/ReplacementForFriends.java
@@ -11,8 +11,11 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.GeoPoint;
@@ -39,16 +42,6 @@ public class ReplacementForFriends extends AppCompatActivity implements SensorEv
         tvSteps = findViewById(R.id.tvSteps);
         tvHikes = findViewById(R.id.tvHikes);
 
-        Button btnStartHiking = findViewById(R.id.btnStartHiking);
-        Button btnStopHiking = findViewById(R.id.btnStopHiking);
-        Button btnTestChart = findViewById(R.id.btnTestChart);
-        Button btnCompareFriends = findViewById(R.id.btnCompareFriends);
-
-        btnStartHiking.setOnClickListener(v -> startHiking());
-        btnStopHiking.setOnClickListener(v -> stopStepCounter());
-        btnTestChart.setOnClickListener(v -> openChart());
-        btnCompareFriends.setOnClickListener(v -> compareFriends());
-
         simpleLocation = setUpSimpleLocation();
 
         // if we can't access the location yet
@@ -57,21 +50,18 @@ public class ReplacementForFriends extends AppCompatActivity implements SensorEv
             SimpleLocation.openSettings(this);
         }
 
-        simpleLocation.setListener(() -> {
-            // new location data has been received and can be accessed
-            double latitude = simpleLocation.getLatitude();
-            double longitude = simpleLocation.getLongitude();
+        setUpStartHiking();
+        setUpStopHiking();
+        setUpChartIntent();
+        setUpCompareFriendsIntent();
+        // just for testing, remove later
+        setUpRename();
+    }
 
-            GeoPoint geoPoint = new GeoPoint(latitude, longitude);
-            if (actualHiking == null) {
-                Log.w(TAG, "No actualHiking init...");
-            } else {
-                actualHiking.addGeoPoint(geoPoint);
-                tvHikes.setText("Added: " + geoPoint.toString() + ", size of actualHiking: " + actualHiking.getGeoPoints().size());
-                Log.d(TAG, "Added: " + geoPoint.toString() + ", size of actualHiking: " + actualHiking.getGeoPoints().size());
-            }
-        });
-
+    private void setUpRename() {
+        Button btnRename = findViewById(R.id.btnRename);
+        EditText name = findViewById(R.id.editTextNameRename);
+        btnRename.setOnClickListener(v -> FireBaseHelper.updateLoggedInUserName(name.getText().toString()));
     }
 
     private SimpleLocation setUpSimpleLocation() {
@@ -84,48 +74,44 @@ public class ReplacementForFriends extends AppCompatActivity implements SensorEv
         return new SimpleLocation(context, requireFineGranularity, passiveMode, updateIntervalInMilliseconds, requireNewLocation);
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
+    private SimpleLocation.Listener defineCustomListener() {
+        return () -> {
+            // new location data has been received and can be accessed
+            double latitude = simpleLocation.getLatitude();
+            double longitude = simpleLocation.getLongitude();
 
-        // make the device update its location
-        simpleLocation.beginUpdates();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-
-        // stop location updates (saves battery)
-        simpleLocation.endUpdates();
-    }
-
-    @Deprecated
-    private void updateUserNameToJustin() {
-        FireBaseHelper.updateLoggedInUserName("Justin");
-    }
-
-    private void compareFriends() {
-        Intent intent = new Intent(this, FriendsList.class);
-        startActivity(intent);
-    }
-
-    private void openChart() {
-        Intent intent = new Intent(this, TestRecentHikings.class);
-        startActivity(intent);
+            GeoPoint geoPoint = new GeoPoint(latitude, longitude);
+            if (actualHiking == null) {
+                tvHikes.setText("No actualHiking init..." + Math.random());
+                Log.w(TAG, "No actualHiking init, should not happen, listener should be detached");
+            } else {
+                actualHiking.addGeoPoint(geoPoint);
+                tvHikes.setText("Added: " + geoPoint.toString() + ", size of actualHiking: " + actualHiking.getGeoPoints().size());
+                Log.d(TAG, "Added: " + geoPoint.toString() + ", size of actualHiking: " + actualHiking.getGeoPoints().size());
+            }
+        };
     }
 
     private void stopStepCounter() {
-        // TODO: 16.05.2019 stop this and stop geo request
         // look if steps < 100
-        // now just add actualHiking and stop it
+        // crashes if hiking is null
+        if (actualHiking == null){
+            Toast.makeText(this, "You haven't even started yet", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "Hiking didn't start");
+            return;
+        }
+
         actualHiking.setSteps(currentSteps);
         actualHiking.setEnd(Timestamp.now());
         // TODO: 16.05.2019 fetch locations and time
 
         FireBaseHelper.addToLoggedInUser(actualHiking);
-        stopStickyNotification();
+        stopStickyNotification(); // also stop step counter
+        simpleLocation.endUpdates();
         // TODO: 22.05.2019 maybe congrats for walking
+
+        tvHikes.setText("Well done! Meters: " + actualHiking.inMeter());
+        actualHiking = null;
     }
 
     private void stopStickyNotification() {
@@ -143,7 +129,6 @@ public class ReplacementForFriends extends AppCompatActivity implements SensorEv
     }
 
     private void startHiking() {
-        // TODO: 20.05.2019 warning if actualHiking is already started
         // let user decide what to because hiking is already started
         if (actualHiking != null) {
             AlertDialog.Builder builder = new AlertDialog.Builder(this)
@@ -167,6 +152,9 @@ public class ReplacementForFriends extends AppCompatActivity implements SensorEv
 
         showStickyNotification();
         startStepCounter();
+
+        simpleLocation.setListener(defineCustomListener()); // should i start?
+        simpleLocation.beginUpdates();
     }
 
     private void startStepCounter() {
@@ -178,9 +166,31 @@ public class ReplacementForFriends extends AppCompatActivity implements SensorEv
             Objects.requireNonNull(sensorManager).registerListener(this, sensor,
                     SensorManager.SENSOR_DELAY_UI);
         } catch (NullPointerException ne) {
-            currentSteps = 4711; // so there is something in database
             Log.w(TAG, "something is null at sensor for steps - happens at emulator", ne);
         }
+    }
+
+    private void setUpStartHiking(){
+        Button btnStartHiking = findViewById(R.id.btnStartHiking);
+        btnStartHiking.setOnClickListener(v -> startHiking());
+    }
+    private void setUpStopHiking(){
+        Button btnStopHiking = findViewById(R.id.btnStopHiking);
+        btnStopHiking.setOnClickListener(v -> stopStepCounter());
+    }
+    private void setUpChartIntent(){
+        Button btnTestChart = findViewById(R.id.btnTestChart);
+        btnTestChart.setOnClickListener(v -> {
+            Intent intent = new Intent(this, TestRecentHikings.class);
+            startActivity(intent);
+        });
+    }
+    private void setUpCompareFriendsIntent(){
+        Button btnCompareFriends = findViewById(R.id.btnCompareFriends);
+        btnCompareFriends.setOnClickListener(v -> {
+            Intent intent = new Intent(this, FriendsList.class);
+            startActivity(intent);
+        });
     }
 
     @Override
@@ -195,6 +205,22 @@ public class ReplacementForFriends extends AppCompatActivity implements SensorEv
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // make the device update its location
+        simpleLocation.beginUpdates();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        // stop location updates (saves battery)
+        simpleLocation.endUpdates();
     }
 
 }
